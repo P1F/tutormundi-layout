9b04ee00a139d55e88bb64b7e612e4d747e2b12e
P1F <rtsouza15@gmail.com>
2021-05-16 05:09:10 -0400

    third commit

--- src/components/GeneralPurpose/RatingIcon.jsx
+++ src/components/GeneralPurpose/RatingIcon.jsx
@@ -9,31 +9,36 @@ const StarIconWrapper = styled.ul`
 
     li {
       display: inline-block;
+      transition: 0.2s;
+
+      &:hover {
+        ${props => props.EditMode && 'transform: scale(1.3);'}
+      }
     }
 `;
 
 function RatingIcon(props) {
   const [hoverRating, setHoverRating] = useState(0);
   const [rating, setRating] = useState(0);
-  const [edit, setEdit] = useState(true);
+  const [EditMode, setEditMode] = useState(true);
 
   function handleHoverRating(idx) {
     setHoverRating(idx);
   }
 
   function handleRating(){
-    setEdit(!edit);
+    setEditMode(!EditMode);
     setRating(hoverRating);
   }
 
   return (
-    <StarIconWrapper>
+    <StarIconWrapper EditMode={EditMode}>
       {[...Array(5)].map((x, i) => {
         return (
           <li onMouseOver={() => handleHoverRating(i+1)}
               onClick={handleRating}
               onMouseLeave={() => handleHoverRating(0)} >
-            <StarIcon filled={(i+1 <= hoverRating && edit) || (i+1 <= rating && !edit)}/>
+            <StarIcon filled={(i+1 <= hoverRating && EditMode) || (i+1 <= rating && !EditMode)}/>
           </li>
         );
       })}
